0c703e9acaea86452e311a26cfb5efaed84e17f3
Raul Parola <parolaraul@gmail.com>
2023-04-20 15:12:46 +0200

    fix tests archstructure

--- src/test/java/com/trackysat/kafka/TechnicalStructureTest.java
+++ src/test/java/com/trackysat/kafka/TechnicalStructureTest.java
@@ -21,13 +21,14 @@ class TechnicalStructureTest {
         .layer("Security").definedBy("..security..")
         .layer("Persistence").definedBy("..repository..")
         .layer("Domain").definedBy("..domain..")
+        .layer("Tasks").definedBy("..tasks..")
 
         .whereLayer("Config").mayNotBeAccessedByAnyLayer()
         .whereLayer("Web").mayOnlyBeAccessedByLayers("Config")
-        .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config")
-        .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Service", "Web")
-        .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service", "Security", "Web", "Config")
-        .whereLayer("Domain").mayOnlyBeAccessedByLayers("Persistence", "Service", "Security", "Web", "Config")
+        .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config", "Tasks")
+        .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Service", "Web", "Tasks")
+        .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service", "Security", "Web", "Config", "Tasks")
+        .whereLayer("Domain").mayOnlyBeAccessedByLayers("Persistence", "Service", "Security", "Web", "Config", "Tasks")
 
         .ignoreDependency(belongToAnyOf(DemoKafkaJHipsterApp.class), alwaysTrue())
         .ignoreDependency(alwaysTrue(), belongToAnyOf(
